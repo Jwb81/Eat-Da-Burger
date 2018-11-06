279889d1d83c46b85b16307e0814d16a27c10a24
Jason Barnett <jayb21@neo.rr.com>
2018-11-06 13:32:29 -0500

    modified table to work with Jaws DB

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,10 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(45),
     devoured BOOLEAN DEFAULT FALSE,
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     PRIMARY KEY (id)
 )
